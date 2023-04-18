fba096ac5cf370865b1cebd99cbf8254954e9d64
louay kharouf <louay.khrouf@insat.ucar.tn>
2023-04-18 12:34:18 +0100

    terraform updated

--- linux.tf
+++ linux.tf
@@ -65,6 +65,19 @@ resource "azurerm_network_security_rule" "nsr-3" {
   resource_group_name         = azurerm_resource_group.resource-group.name
   network_security_group_name = azurerm_network_security_group.network-security-group.name
 }
+resource "azurerm_network_security_rule" "nsr-2" {
+  name                        = "HTTP"
+  priority                    = 150
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.resource-group.name
+  network_security_group_name = azurerm_network_security_group.network-security-group.name
+}
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "Todo-App-public-ip"
